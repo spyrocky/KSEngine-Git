e3ae2873b52df5a76b00ac4e157ba0e75a92b871
Pimkamonw <kamonwan.srisawad@gmail.com>
2023-03-25 23:01:37 +1100

    Update Camera.h

--- KSEngine/includes/KSEngine/Graphics/Camera.h
+++ KSEngine/includes/KSEngine/Graphics/Camera.h
@@ -1,10 +1,30 @@
 #pragma once
 #include "KSEngine/Math/Transformations.h"
 
+struct STCameraData {
+	//how fast the camera can move
+	float Speed = 5.0f;
+	//Basically the zoom
+	//How much of view space the camera can see - in degree
+	// think pereipheral vision
+	float FOV = 70.0f;
+	//how close can models get to the camera before disappearing
+	float NearClip = 0.01f;
+	// * opposite of near clip *
+	float FarClip = 1000.0f;
+};
+
 class Camera {
 public:
 	Camera();
 	~Camera();
+
+	//return the transform data
+	CTransform GetTransforms() const { return Transform; }
+	//return the current facing directions
+	CDirection GetDirection() const { return Direction; }
+
+	//STCameraData 
 
 private:
 	// trasform of the camenra
@@ -13,14 +33,7 @@ private:
 
 	//forward , right , up directions
 	CDirection Direction;
-	//how fast the camera can move
-	float Speed;
-	//Basically the zoom
-	//How much of view space the camera can see - in degree
-	// think pereipheral vision
-	float FOV;
-	//how close can models get to the camera before disappearing
-	float NearClip;
-	// * opposite of near clip *
-	float FarClip;
+	
+	// hold all the camera effect
+	STCameraData Cameradata;
 };
